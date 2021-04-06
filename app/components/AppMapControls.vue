e6531ebd327a74e6896104f7a6be57a49bb0aa8b
Felix Fennell <felnne@bas.ac.uk>
2021-04-06 14:15:02 +0100

    Fixing typo in watcher condition

--- app/components/AppMapControls.vue
+++ app/components/AppMapControls.vue
@@ -98,7 +98,7 @@ export default {
       }
     },
     rotation_longitude: function () {
-      if (this.rotation_source == 'heading') {
+      if (this.rotation_source == 'longitude') {
         this.rotation_degrees = this.rotation_longitude;
       }
     },
