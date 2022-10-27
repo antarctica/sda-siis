d29d131f51c1aff31c8bacbb5ece0165f8d484c5
Felix Fennell <felnne@bas.ac.uk>
2022-10-27 13:53:37 +0100

    Hiding time controls for single granule selection products

--- app/components/AppProductTimeFilter.vue
+++ app/components/AppProductTimeFilter.vue
@@ -1,37 +1,35 @@
 <template>
   <fieldset class="time-filters">
-    <label for="time-filter">Time Filter</label>
-    <button
-      v-on:click="time_filter = -1"
-      :disabled="time_filter == 'disabled' ? 'disabled' : null"
-      :class="time_filter == -1 ? 'activated': null"
-    >
-      None
-    </button>
-    <button
-      v-on:click="time_filter = 72"
-      :disabled="time_filter == 'disabled' ? 'disabled' : null"
-      :class="time_filter == 72 ? 'activated': null"
-    >
-      72 H
-    </button>
-    <button
-      v-on:click="time_filter = 48"
-      :disabled="time_filter == 'disabled' ? 'disabled' : null"
-      :class="time_filter == 48 ? 'activated': null"
-    >
-      48 H
-    </button>
-    <button
-      v-on:click="time_filter = 24"
-      :disabled="time_filter == 'disabled' ? 'disabled' : null"
-      :class="time_filter == 24 ? 'activated': null"
-    >
-      24 H
-    </button>
-    <br />
-    <label for="date-filter">Date filter</label>
-    <input type="date" v-model="date_filter"/>
+    <template v-if="granules_selection_mode == 'multiple'">
+      <button
+        v-on:click="time_filter = -1"
+        :disabled="time_filter == 'disabled' ? 'disabled' : null"
+        :class="time_filter == -1 ? 'activated': null"
+      >
+        All
+      </button>
+      <button
+        v-on:click="time_filter = 72"
+        :disabled="time_filter == 'disabled' ? 'disabled' : null"
+        :class="time_filter == 72 ? 'activated': null"
+      >
+        72 H
+      </button>
+      <button
+        v-on:click="time_filter = 48"
+        :disabled="time_filter == 'disabled' ? 'disabled' : null"
+        :class="time_filter == 48 ? 'activated': null"
+      >
+        48 H
+      </button>
+      <button
+        v-on:click="time_filter = 24"
+        :disabled="time_filter == 'disabled' ? 'disabled' : null"
+        :class="time_filter == 24 ? 'activated': null"
+      >
+        24 H
+      </button>
+    </template>
     <input type="date" v-model="date_filter" :min="min_date" :max="max_date" />
   </fieldset>
 </template>
@@ -46,13 +44,15 @@ export default {
   },
 
   props: [
+    'granules_selection_mode',
     'default_time_filter',
     'min_date',
     'max_date'
   ],
 
   watch: {
-    date_filter: function () {      if (this.date_filter !== "") {
+    date_filter: function () {
+      if (this.date_filter !== "") {
         this.time_filter = 'disabled';
       } else {
         this.time_filter = 0;
