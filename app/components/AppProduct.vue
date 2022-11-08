412195f3309aa35452c770191556bd95f4029c46
Felix Fennell <felnne@bas.ac.uk>
2022-11-08 14:54:11 +0000

    Fixing granule selection order

--- app/components/AppProduct.vue
+++ app/components/AppProduct.vue
@@ -241,7 +241,7 @@ export default {
         this.calculateTemporalExtent();
         if (this.granules_selection_mode === 'single') {
           if (this.granules.length > 0) {
-            this.selected_granule_indexes = [this.granules.length - 1];
+            this.selected_granule_indexes = [0];
           }
         }
       }
@@ -383,7 +383,7 @@ export default {
         this.calculateTemporalExtent();
 
         if (this.granules.length > 0 && this.granules_selection_mode === 'single') {
-          this.selected_granule_indexes = [this.granules[0]];
+          this.selected_granule_indexes = [0];
         }
       }
     },
