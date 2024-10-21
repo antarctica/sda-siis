0d774e44b9a0ccbecf3422528a3a86bece05f092
Jonny Dawe <jonwe@bas.ac.uk>
2024-10-21 14:18:57 +0100

    Add Open-api types integration process - able to generate and consume api type with swr hooks.

--- app_v2/src/vite-env.d.ts
+++ app_v2/src/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_CACHE_ID: string;
+  readonly VITE_CACHE_ID: string; // Cache ID for the application
+  readonly VITE_SERVICE_API_ENDPOINT: string; // Service API endpoint
+  readonly VITE_SERVICE_API_OGC_ENDPOINT: string; // Service API OGC endpoint
 }
 
 interface ImportMeta {
