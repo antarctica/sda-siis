949f512eab500743b465720404865c16b87cb3fb
Jonny Dawe <jonwe@bas.ac.uk>
2024-12-09 12:48:53 +0000

    Update Vite configuration to load environment variables and transform index.html for dynamic base URL; add VITE_BASE_URL to .env.development for improved deployment flexibility.

--- app_v2/vite.config.ts
+++ app_v2/vite.config.ts
@@ -1,19 +1,49 @@
 /// <reference types="vitest" />
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({ tsDecorators: true }), tsconfigPaths(), TanStackRouterVite()],
-  server: {
-    host: true,
-    port: 5173,
-  },
-  test: {
-    globals: true,
-    setupFiles: './src/test/setup.tsx',
-    environment: 'jsdom',
-  },
+export default defineConfig(function ({ mode }) {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react({ tsDecorators: true }),
+      tsconfigPaths(),
+      TanStackRouterVite(),
+      {
+        // This plugin is used to transform the index.html file to add the correct base URL and og:image and twitter:image
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          const baseUrl = env.VITE_BASE_URL || 'https://siis.example.com';
+
+          const transformed = html
+            .replace(
+              /<meta\s+property="og:url"\s+content="[^"]*"\s*\/?>/i,
+              `<meta property="og:url" content="${baseUrl}" />`,
+            )
+            .replace(
+              /<meta\s+property="og:image"\s+content="[^"]*"\s*\/?>/i,
+              `<meta property="og:image" content="${baseUrl}/og-image.png" />`,
+            )
+            .replace(
+              /<meta\s+name="twitter:image"\s+content="[^"]*"\s*\/?>/i,
+              `<meta name="twitter:image" content="${baseUrl}/og-image.png" />`,
+            );
+
+          return transformed;
+        },
+      },
+    ],
+    server: {
+      host: true,
+      port: 5173,
+    },
+    test: {
+      globals: true,
+      setupFiles: './src/test/setup.tsx',
+      environment: 'jsdom',
+    },
+  };
 });
